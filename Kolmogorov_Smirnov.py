10906461943f849e0baf72814a9e1e541a16aff7
Juan Daniel <danielalejandre1050@gmail.com>
2022-03-15 14:32:16 -0600

    Kolmogorov finished

--- Kolmogorov_Smirnov.py
+++ Kolmogorov_Smirnov.py
@@ -1,9 +1,13 @@
+from curses import keyname
 import random
 from prettytable import PrettyTable
+from Estadisticos import statistic_table_alfa_10, statistic_table_alfa_5, statistic_table_alfa_1
 
 def main():
     random_numbers = []
     n = getInput(prompt="Ingrese la cantidad de numeros aleatorios a generar: ", cast=int,
+                 condition=lambda x: x > 0, errorMessage="El valor debe ser mayor a cero. Intenta de nuevo.")
+    alfa = getInput(prompt="Ingrese el valor porcentual de alfa (en entero): ", cast=int,
                  condition=lambda x: x > 0, errorMessage="El valor debe ser mayor a cero. Intenta de nuevo.")
     for rnd in range(0, n):
         number = round(random.uniform(0, 1), 5)
@@ -23,7 +27,64 @@ def main():
 
     max_Dn = max(Dn)
     print(table)
-    print("Valor mayor de estadisticos calculados", round(max_Dn, 5))
+
+    if(alfa == 10):
+        statisticResult_alfa10(n, alfa, max_Dn)
+    elif(alfa == 5):
+        statisticResult_alfa5(n, alfa, max_Dn)
+    elif(alfa == 1):
+        statisticResult_alfa1(n, alfa, max_Dn)
+
+
+def statisticResult_alfa10(n, alfa, max_Dn):
+    key = "alfa"
+    res = [val[key] for keys, val in statistic_table_alfa_10.items() if key in val]
+    statistic_table_value = statistic_table_alfa_10[n].get('dis')
+
+    for k, v in statistic_table_alfa_10.items():
+        if(k == n and res[k] == alfa):
+                print("Valor mayor de estadisticos calculados:", round(max_Dn, 5))
+                print("Estadistico de tablas:", statistic_table_value)
+                print("" + str(max_Dn) + " < " + str(statistic_table_value) + "")
+
+    if(max_Dn < statistic_table_value):
+        print("Los numeros son aceptados.")
+    else:
+        print("Los numeros no son aceptados.")
+
+
+def statisticResult_alfa5(n, alfa, max_Dn):
+    key = "alfa"
+    res = [val[key] for keys, val in statistic_table_alfa_5.items() if key in val]
+    statistic_table_value = statistic_table_alfa_5[n].get('dis')
+
+    for k, v in statistic_table_alfa_5.items():
+        if(k == n and res[k] == alfa):
+                print("Valor mayor de estadisticos calculados:", round(max_Dn, 5))
+                print("Estadistico de tablas:", statistic_table_value)
+                print("" + str(max_Dn) + " < " + str(statistic_table_value) + "")
+
+    if(max_Dn < statistic_table_value):
+        print("Los numeros son aceptados.")
+    else:
+        print("Los numeros no son aceptados.")
+
+
+def statisticResult_alfa1(n, alfa, max_Dn):
+    key = "alfa"
+    res = [val[key] for keys, val in statistic_table_alfa_1.items() if key in val]
+    statistic_table_value = statistic_table_alfa_1[n].get('dis')
+
+    for k, v in statistic_table_alfa_10.items():
+        if(k == n and res[k] == alfa):
+                print("Valor mayor de estadisticos calculados:", round(max_Dn, 5))
+                print("Estadistico de tablas:", statistic_table_value)
+                print("" + str(max_Dn) + " < " + str(statistic_table_value) + "")
+
+    if(max_Dn < statistic_table_value):
+        print("Los numeros son aceptados.")
+    else:
+        print("Los numeros no son aceptados.")
 
 
 def getInput(prompt="", cast=None, condition=None, errorMessage=None):
